be3eab00a20908cc10f890811dd45799ed9272e6
ChrisB85 <krzysztof.blachowicz@gmail.com>
2025-06-12 08:47:34 +0000

    RPI5 fixes

--- node_helper.js
+++ node_helper.js
@@ -1,216 +1,192 @@
 'use strict';
 
 /* Magic Mirror
-* Module: MMM-PIR-Sensor
-*
-* By Paul-Vincent Roll http://paulvincentroll.com
-* MIT Licensed.
-*/
+ * Module: MMM-PIR-Sensor (node-libgpiod + xrandr only)
+ *
+ * By Paul-Vincent Roll http://paulvincentroll.com
+ * MIT Licensed.
+ */
 
 const NodeHelper = require('node_helper');
-const Gpio = require('onoff').Gpio;
+const { Chip, Line } = require('node-libgpiod');
 const exec = require('child_process').exec;
 
 module.exports = NodeHelper.create({
-    start: function () {
-        this.started = false;
-        console.log('[PIR-Sensor] Module started');
-    },
+  start() {
+    this.started = false;
+    console.log('[PIR-Sensor] Module started');
+  },
 
-    activateMonitor: function () {
-        console.log('[PIR-Sensor] Attempting to activate monitor');
-        // If always-off is enabled, keep monitor deactivated
-        let alwaysOffTrigger = this.alwaysOff && (this.alwaysOff.readSync() === this.config.alwaysOffState)
-        if (alwaysOffTrigger) {
-            console.log('[PIR-Sensor] Monitor activation blocked by always-off trigger');
-            return;
-        }
-        // If relays are being used in place of HDMI
-        if (this.config.relayPin !== false) {
-            this.relay.writeSync(this.config.relayState);
-        }
-        else if (this.config.relayPin === false) {
-            // Check if hdmi output is already on
-            const self = this;
-            exec("sudo /usr/bin/vcgencmd display_power").stdout.on('data', function(data) {
-		if (data.indexOf("display_power=0") === 0) {
-                    exec("sudo /usr/bin/vcgencmd display_power 1", null);
-	 	}
- 	        if (self.config.supportCEC)
-    	            exec("echo 'on 0' | cec-client -s -d 1");
-            });
-        }
-	if (this.briefHDMIWakeupInterval) {
-	    clearInterval(this.briefHDMIWakeupInterval);
-	    clearTimeout(this.briefHDMIWakeupPhase2Timeout);
-	    this.briefHDMIWakeupInterval = null;
-        }
-    },
-
-    deactivateMonitor: function () {
-        // If always-on is enabled, keep monitor activated
-        let alwaysOnTrigger = this.alwaysOn && (this.alwaysOn.readSync() === this.config.alwaysOnState)
-        let alwaysOffTrigger = this.alwaysOff && (this.alwaysOff.readSync() === this.config.alwaysOffState)
-        if (alwaysOnTrigger && !alwaysOffTrigger) {
-            return;
-        }
-        // If relays are being used in place of HDMI
-        if (this.config.relayPin !== false) {
-            this.relay.writeSync((this.config.relayState + 1) % 2);
-        }
-        else if (this.config.relayPin === false) {
-	    if (this.config.supportCEC)
-	        exec("echo 'standby 0' | cec-client -s -d 1");
-            exec("sudo /usr/bin/vcgencmd display_power 0", null);
-        }
-	if (this.config.preventHDMITimeout > 0 && this.config.preventHDMITimeout < 10) {
-            const self = this;
-	    self.briefHDMIWakeupInterval = setInterval(function() {
-                self.briefHDMIWakeup();
-        	}, self.config.preventHDMITimeout * 1000 * 60);
-	}
-    },
-
-    briefHDMIWakeup: function() {
-	const self = this
-        exec("/usr/bin/vcgencmd display_power").stdout.on('data', function(data) {
-            if (data.indexOf("display_power=0") === 0) {
-                exec("/usr/bin/vcgencmd display_power 1", null);
-
-                self.briefHDMIWakeupPhase2Timeout = setTimeout(function() { // set a short delay for the monitor to sense the input
-                    self.briefHDMIWakeupPhase2Timeout = null;
-                    exec("/usr/bin/vcgencmd display_power 0", null);
-                }, 1000);
-            }
-        });
-    },
-
-    // Subclass socketNotificationReceived received.
-    socketNotificationReceived: function (notification, payload) {
-        if (notification === 'CONFIG' && this.started == false) {
-            const self = this;
-            this.config = payload;
-            console.log('[PIR-Sensor] Received configuration:', JSON.stringify(this.config));
-
-	    if (self.config.powerSaving) {
-                self.deactivateMonitorTimeout = setTimeout(function() { // Set the timeout before movement is identified
-                    self.deactivateMonitor();
-                }, self.config.powerSavingDelay * 1000);
-            }
-
-            // Setup for relay pin
-            if (this.config.relayPin) {
-                this.relay = new Gpio(this.config.relayPin, 'out');
-                this.relay.writeSync(this.config.relayState);
-                exec("/usr/bin/vcgencmd display_power 1", null);
-	        if (this.config.supportCEC)
-    	            exec("echo 'on o' | cec-client -s -d 1");
-            }
-
-            // Setup for alwaysOn switch
-            if (this.config.alwaysOnPin) {
-                this.alwaysOn = new Gpio(this.config.alwaysOnPin, 'in', 'both');
-                const alwaysOnState = this.config.alwaysOnState
-                this.alwaysOn.watch(function (err, value) {
-                    if (value === alwaysOnState) {
-                        self.sendSocketNotification('ALWAYS_ON', true);
-                        self.sendSocketNotification('SHOW_ALERT', {
-                            title: 'Always-On Activated',
-                            message: 'Mirror will not activate power-saving mode',
-                            timer: 4000
-                        });
-                        if (self.config.powerSaving){
-                            clearTimeout(self.deactivateMonitorTimeout);
-                        }
-                    } else if (value === (alwaysOnState + 1) % 2) {
-                        self.sendSocketNotification('ALWAYS_ON', false);
-                        self.sendSocketNotification('SHOW_ALERT', {
-                            title: 'Always-On Deactivated',
-                            message: 'Mirror will now use motion sensor to activate',
-                            timer: 4000
-                        });
-                    }
-                })
-            }
-
-            // Setup for alwaysOff switch
-            if (this.config.alwaysOffPin) {
-                this.alwaysOff = new Gpio(this.config.alwaysOffPin, 'in', 'both');
-                const alwaysOffState = this.config.alwaysOffState
-                this.alwaysOff.watch(function (err, value) {
-                    if (value === alwaysOffState) {
-                        self.sendSocketNotification('ALWAYS_OFF', true);
-                        self.deactivateMonitor();
-                    } else if (value === (alwaysOffState + 1) % 2) {
-                        self.sendSocketNotification('ALWAYS_OFF', false);
-                        self.activateMonitor();
-                        if (self.config.powerSaving){
-                            clearTimeout(self.deactivateMonitorTimeout);
-                        }
-                    }
-                })
-            }
-	    else {
-                exec("/usr/bin/vcgencmd display_power 1", null);  // Mirror could have stopped with HDMI off. Reset at startup
-	        if (this.config.supportCEC)
-    	            exec("echo 'on o' | cec-client -s -d 1");
-	    }
-
-            // Setup for sensor pin
-            try {
-                console.log('[PIR-Sensor] Initializing PIR sensor on GPIO pin:', this.config.sensorPin);
-                this.pir = new Gpio(this.config.sensorPin, 'in', 'both');
-                console.log('[PIR-Sensor] PIR sensor initialized successfully');
-            } catch (error) {
-                console.error('[PIR-Sensor] Failed to initialize PIR sensor:', error.message);
-                return;
-            }
-
-            // Setup value which represent on and off
-            const valueOn = this.config.sensorState;
-            const valueOff = (this.config.sensorState + 1) % 2;
-
-            // Detected movement
-            this.pir.watch(function (err, value) {
-                if (err) {
-                    console.error('[PIR-Sensor] Error reading PIR sensor:', err.message);
-                    return;
-                }
-                
-                console.log('[PIR-Sensor] Sensor state changed:', value === valueOn ? 'MOVEMENT DETECTED' : 'NO MOVEMENT');
-                
-                if (value == valueOn) {
-                    self.sendSocketNotification('USER_PRESENCE', true);
-                    if (self.config.powerSaving){
-                        clearTimeout(self.deactivateMonitorTimeout);
-                        self.activateMonitor();
-                    }
-                }
-                else if (value == valueOff) {
-                    self.sendSocketNotification('USER_PRESENCE', false);
-                    if (!self.config.powerSaving){
-                        return;
-                    }
-
-                    self.deactivateMonitorTimeout = setTimeout(function() {
-                        self.deactivateMonitor();
-                    }, self.config.powerSavingDelay * 1000);
-                }
-            });
-
-            this.started = true;
-
-	    if (this.config.runSimulator) {
-	    	setInterval(function(){ 
-			self.sendSocketNotification('USER_PRESENCE', true);
-			setTimeout(function() {
-				self.sendSocketNotification('USER_PRESENCE', false);
-			}, 1000);
-		    }, 20000);
-	    }
-        } else if (notification === 'SCREEN_WAKEUP') {
-            this.activateMonitor();
-        }
+  activateMonitor() {
+    console.log('[PIR-Sensor] Attempting to activate monitor');
+    // always-off override
+    if (this.alwaysOffLine && this.alwaysOffLine.getValue() === this.config.alwaysOffState) {
+      console.log('[PIR-Sensor] Monitor activation blocked by always-off trigger');
+      return;
     }
 
+    if (this.relayLine) {
+      this.relayLine.setValue(this.config.relayState);
+    } else {
+      exec("xrandr --display :0 --output HDMI-1 --auto");
+    }
+
+    clearInterval(this.briefHDMIWakeupInterval);
+    clearTimeout(this.briefHDMIWakeupPhase2Timeout);
+    this.briefHDMIWakeupInterval = null;
+  },
+
+  deactivateMonitor() {
+    // always-on override
+    if (
+      this.alwaysOnLine &&
+      this.alwaysOnLine.getValue() === this.config.alwaysOnState &&
+      !(this.alwaysOffLine && this.alwaysOffLine.getValue() === this.config.alwaysOffState)
+    ) {
+      return;
+    }
+
+    if (this.relayLine) {
+      this.relayLine.setValue((this.config.relayState + 1) % 2);
+    } else {
+      exec("xrandr --display :0 --output HDMI-1 --off");
+    }
+
+    if (this.config.preventHDMITimeout > 0 && this.config.preventHDMITimeout < 10) {
+      this.briefHDMIWakeupInterval = setInterval(() => {
+        this.briefHDMIWakeup();
+      }, this.config.preventHDMITimeout * 60 * 1000);
+    }
+  },
+
+  briefHDMIWakeup() {
+    // zamiast vcgencmd: raz włączamy, po chwili wyłączamy
+    exec("xrandr --display :0 --output HDMI-1 --auto");
+    this.briefHDMIWakeupPhase2Timeout = setTimeout(() => {
+      exec("xrandr --display :0 --output HDMI-1 --off");
+    }, 1000);
+  },
+
+  socketNotificationReceived(notification, payload) {
+    if (notification === 'CONFIG' && !this.started) {
+      const self = this;
+      this.config = payload;
+      console.log('[PIR-Sensor] Received configuration:', JSON.stringify(this.config));
+
+      // inicjalizacja libgpiod
+      this.chip = new Chip(0);
+
+      // relayPin jako output
+      if (this.config.relayPin !== false) {
+        this.relayLine = new Line(this.chip, this.config.relayPin);
+        this.relayLine.requestOutputMode();
+        this.relayLine.setValue(this.config.relayState);
+      }
+
+      // alwaysOnPin jako input (polling)
+      if (this.config.alwaysOnPin) {
+        this.alwaysOnLine = new Line(this.chip, this.config.alwaysOnPin);
+        this.alwaysOnLine.requestInputMode();
+        this.prevAlwaysOn = this.alwaysOnLine.getValue();
+        setInterval(() => {
+          const v = this.alwaysOnLine.getValue();
+          if (v !== this.prevAlwaysOn) {
+            const on = v === this.config.alwaysOnState;
+            self.sendSocketNotification('ALWAYS_ON', on);
+            self.sendSocketNotification('SHOW_ALERT', {
+              title: on ? 'Always-On Activated' : 'Always-On Deactivated',
+              message: on
+                ? 'Mirror will not activate power-saving mode'
+                : 'Mirror will now use motion sensor to activate',
+              timer: 4000
+            });
+            if (self.config.powerSaving && on) clearTimeout(self.deactivateMonitorTimeout);
+            this.prevAlwaysOn = v;
+          }
+        }, 200);
+      }
+
+      // alwaysOffPin jako input (polling)
+      if (this.config.alwaysOffPin) {
+        this.alwaysOffLine = new Line(this.chip, this.config.alwaysOffPin);
+        this.alwaysOffLine.requestInputMode();
+        this.prevAlwaysOff = this.alwaysOffLine.getValue();
+        setInterval(() => {
+          const v = this.alwaysOffLine.getValue();
+          if (v !== this.prevAlwaysOff) {
+            const off = v === this.config.alwaysOffState;
+            self.sendSocketNotification('ALWAYS_OFF', off);
+            if (off) {
+              self.deactivateMonitor();
+            } else {
+              self.activateMonitor();
+              if (self.config.powerSaving) clearTimeout(self.deactivateMonitorTimeout);
+            }
+            this.prevAlwaysOff = v;
+          }
+        }, 200);
+      } else {
+        // brak alwaysOffPin – wymuś włączenie przy starcie
+        this.activateMonitor();
+      }
+
+      // powerSaving: wstępny timeout na wyłączenie
+      if (this.config.powerSaving) {
+        this.deactivateMonitorTimeout = setTimeout(
+          () => this.deactivateMonitor(),
+          this.config.powerSavingDelay * 1000
+        );
+      }
+
+      // PIR sensor jako input (polling)
+      try {
+        console.log('[PIR-Sensor] Initializing PIR sensor on GPIO pin:', this.config.sensorPin);
+        this.sensorLine = new Line(this.chip, this.config.sensorPin);
+        this.sensorLine.requestInputMode();
+        console.log('[PIR-Sensor] PIR sensor initialized successfully');
+      } catch (error) {
+        console.error('[PIR-Sensor] Failed to initialize PIR sensor:', error.message);
+        return;
+      }
+
+      this.prevSensor = this.sensorLine.getValue();
+      const valueOn = this.config.sensorState;
+      setInterval(() => {
+        const v = this.sensorLine.getValue();
+        if (v !== this.prevSensor) {
+          console.log('[PIR-Sensor] Sensor state changed:', v === valueOn ? 'MOVEMENT DETECTED' : 'NO MOVEMENT');
+          if (v === valueOn) {
+            self.sendSocketNotification('USER_PRESENCE', true);
+            if (self.config.powerSaving) {
+              clearTimeout(self.deactivateMonitorTimeout);
+              self.activateMonitor();
+            }
+          } else {
+            self.sendSocketNotification('USER_PRESENCE', false);
+            if (self.config.powerSaving) {
+              this.deactivateMonitorTimeout = setTimeout(
+                () => self.deactivateMonitor(),
+                this.config.powerSavingDelay * 1000
+              );
+            }
+          }
+          this.prevSensor = v;
+        }
+      }, 200);
+
+      // opcjonalny symulator ruchu
+      if (this.config.runSimulator) {
+        setInterval(() => {
+          self.sendSocketNotification('USER_PRESENCE', true);
+          setTimeout(() => self.sendSocketNotification('USER_PRESENCE', false), 1000);
+        }, 20000);
+      }
+
+      this.started = true;
+    }
+    else if (notification === 'SCREEN_WAKEUP') {
+      this.activateMonitor();
+    }
+  }
 });
+
